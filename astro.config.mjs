09f46f03b29f0b53d4af7d9de8f50bbd2302492c
Darrion Mills <dmills1994@gmail.com>
2022-10-12 10:07:58 -0400

    build: add data for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
-      external: ["svgo"],
-    },
+      external: ["svgo"]
+    }
   },
+  site: "https://dmills.dev",
+  integrations: [sitemap()]
 });
