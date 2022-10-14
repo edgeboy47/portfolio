f6a78b7b6801a2809577ecf0070e8ee0ec6dc312
Darrion Mills <dmills1994@gmail.com>
2022-10-13 20:19:33 -0400

    build: add plugin for asset compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from 'astro/config';
-
 import sitemap from "@astrojs/sitemap";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,5 @@ export default defineConfig({
     }
   },
   site: "https://dmills.dev",
-  integrations: [sitemap()]
+  integrations: [sitemap(), compress()]
 });
