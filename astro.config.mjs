6cf4f492655117455ade30a1f1db7980285f007d
Darrion Mills <dmills1994@gmail.com>
2022-10-14 22:50:34 -0400

    build: optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
-
 import compress from "astro-compress";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     ssr: {
-      external: ["svgo"]
-    }
+      external: ["svgo"],
+    },
   },
   site: "https://dmills.dev",
-  integrations: [sitemap(), compress()]
+  integrations: [
+    sitemap(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    compress(),
+  ],
 });
